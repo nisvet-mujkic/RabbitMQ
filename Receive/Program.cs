eb789e66ae0e579ac67968b4b47297143d175849
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2023-01-10 16:03:17 +0100

    basic hello world example

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    public class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] received {message}");
+            };
+
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [ENTER] to exit");
+            Console.ReadLine();
+        }
+    }
+}
--- Send/Program.cs
+++ Send/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    public class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+            Console.WriteLine($" [x] sent '{message}'");
+            Console.WriteLine("Press [ENTER] to exit");
+            Console.ReadLine();
+        }
+
+    }
+}
